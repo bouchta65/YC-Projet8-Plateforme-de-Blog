fd2cd2eae9189433d4b124b2b9b6af43ddc45984
bouchta65 <bouchtamohamed01@gmail.com>
2024-12-20 04:04:17 +0100

    Display archived articles and add functionality to delete or unarchive articles

--- src/views/Archive.php
+++ src/views/Archive.php
@@ -8,6 +8,7 @@ if (isset($_SESSION["user"]) && $_SESSION["role"] == "Admin") {
 }
 
 include "statistiquesAdmin.php";
+include "AddArticle.php";
 
 ?>
 <!DOCTYPE html>
@@ -51,7 +52,7 @@ include "statistiquesAdmin.php";
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
           </svg>
         </a>
-        <a href="blablack_listeck"  class="inline-flex items-center justify-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
+        <a href="black_liste.php"  class="inline-flex items-center justify-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
             <span class="sr-only">Blacklist</span>
             <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 11l3-3m0 0l3 3m-3-3v6m6-6a9 9 0 11-12 0 9 9 0 0112 0z" />
@@ -59,12 +60,12 @@ include "statistiquesAdmin.php";
         </a>
 
         <a href="Archive.php"  class="inline-flex items-center justify-center py-3 text-purple-600 bg-white rounded-lg">
-        <span class="sr-only">Archive</span>
+        <span class="sr-only">Tags</span>
         <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 7h10M5 10h14M6 13h12m2-5a1 1 0 011 1v10a1 1 0 01-1 1H5a1 1 0 01-1-1V9a1 1 0 011-1h14z" />
         </svg>
         </a>
-
+   
       </nav>
       <div class="inline-flex items-center justify-center h-20 w-20 border-t border-gray-700">
         <button class="p-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
@@ -116,9 +117,8 @@ include "statistiquesAdmin.php";
     <main class="p-6 sm:p-10 space-y-6">
     <div class="flex flex-col space-y-6 md:space-y-0 md:flex-row justify-between">
         <div class="mr-6">
-          <h1 class="text-4xl font-semibold mb-2">User Information
-          </h1>
-          <h2 class="text-gray-600 ml-0.5">Welcome to your Blog Dashboard</h2>
+          <h1 class="text-4xl font-semibold mb-2">Archived Articles</h1>
+          <h2 class="text-gray-600 ml-0.5">List of articles that have been archived</h2>
         </div>
         <div class="flex flex-wrap items-start justify-end -mb-3">     
           <button id="addatrticle" class="inline-flex px-5 py-3 text-white bg-purple-600 hover:bg-purple-700 focus:bg-purple-700 rounded-md ml-6 mb-3">
@@ -130,143 +130,123 @@ include "statistiquesAdmin.php";
         </div>
       </div>
 
-      <div class="container mx-auto">
 
-    <div class="overflow-x-auto">
-      <table class="min-w-full bg-white border border-gray-300 rounded-lg shadow-lg">
-        <thead>
-          <tr class="bg-gray-800 text-white">
-            <th class="py-3 px-4 text-left">ID</th>
-            <th class="py-3 px-4 text-left">Name</th>
-            <th class="py-3 px-4 text-left">Email</th>
-            <th class="py-3 px-4 text-left">Role</th>
-            <th class="py-3 px-4 text-center">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-            <?php 
-            
-            function afficheUsers($conn){
-              $sqll = "SELECT Id_User FROM User WHERE Email_User = '".$_SESSION["user"]."'";
-              $resultt = mysqli_query($conn,$sqll);
-              $Admindata = mysqli_fetch_row($resultt);
-              $idAdmin = $Admindata[0];
-              
-            $sql = "SELECT * FROM User where Id_User<>1 and Id_User <>$idAdmin";
-            $result = mysqli_query($conn,$sql);
-            while($row = mysqli_fetch_row($result)){
-                echo '<tr class="hover:bg-gray-100">
-                <td class="py-2 px-4 border-b">'.$row[0].'</td>
-                <td class="py-2 px-4 border-b">'.$row[1].'</td>
-                <td class="py-2 px-4 border-b">'.$row[3].'</td>
-                <td class="py-2 px-4 border-b">'.$row[4].'</td>
-                <td class="py-2 px-4 border-b text-center">
-                  <div class="flex justify-center space-x-4">
-                   <form method="POST" action="" >
-                    <button type="submit" value="'.$row[0].'" class="text-blue-500 hover:text-blue-700" name="Edit" title="Edit">
-                      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 3.487a3.375 3.375 0 014.774 4.774l-9.84 9.84a2.25 2.25 0 01-1.011.564l-3.486.873a.75.75 0 01-.91-.91l.873-3.486a2.25 2.25 0 01.564-1.011l9.84-9.84z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.25 8.25L15.75 4.75" />
-                      </svg>
-                    </button>
-                    <button type="submit" class="text-red-500 hover:text-red-700" value="'.$row[0].'" name="Delete" title="Delete">
-                      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 9.75l-.664 9.345A2.25 2.25 0 0116.594 21H7.406a2.25 2.25 0 01-2.242-1.905L4.5 9.75m9.75-4.5v-1.5a2.25 2.25 0 10-4.5 0v1.5m11.25 0h-18" />
-                      </svg>
-                    </button>
-                     </form>
-                  </div>
-                </td>
-              </tr>';
-            }
-        }
-            if(isset($_POST['Delete'])){
-                $id_user = $_POST['Delete'];
-                $sql = "DELETE from User where Id_User='$id_user'";
-                mysqli_query($conn,$sql);
-            }
-            afficheUsers($conn);
-            
-            ?>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <?php
+      
+        function afficheArtcile($conn){
+          $sqll = "SELECT Id_User FROM User WHERE Email_User = '".$_SESSION["user"]."'";
+          $resultt = mysqli_query($conn,$sqll);
+          $Admindata = mysqli_fetch_row($resultt);
+          $idAdmin = $Admindata[0];
+        $sql = "SELECT * FROM Archive where Id_User='$idAdmin'";
+        $result = mysqli_query($conn,$sql);
+        while($row = mysqli_fetch_row($result)){
+         
+          echo '<div class="w-50 bg-white shadow-md rounded-lg overflow-hidden">
+          <div class="relative">
+            <img src="../../public/'.$row[5].'" alt="Article Image" 
+                 class="w-full h-40 object-cover">
+            <div class="absolute top-3 right-3 flex space-x-2">
+              <form method="POST" action="" >
+                <button type="submit" name="return" title="return" value="'.$row[1].'" class="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded-full shadow">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M18 15l4-4m0 0l-4-4m4 4H3" />
+                </svg>
+                </button>
+              <!-- Bouton Supprimer -->
+              <button type="submit" name="Delete" title="Delete" value="'.$row[1].'" class="bg-red-500 hover:bg-red-700 text-white p-2 rounded-full shadow">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 9.75l-.664 9.345A2.25 2.25 0 0116.594 21H7.406a2.25 2.25 0 01-2.242-1.905L4.5 9.75m9.75-4.5v-1.5a2.25 2.25 0 10-4.5 0v1.5m11.25 0h-18" />
+                </svg>
+              </button>
+              </form>
+            </div>
+          </div>
+          <div class="p-4">
+            <h2 class="text-lg font-semibold text-gray-800 mb-2">'.$row[3].'</h2>
+            <p class="text-sm text-gray-500">Publié le : '.$row[2].'</p>
+          </div>
+        </div>';
+      }
+    }
+    if (isset($_POST['Delete'])) {
+        $id_Article = $_POST['Delete'];
     
-        </tbody>
-      </table>
-    </div>
-  </div>
+        $sql = "DELETE FROM Article WHERE Id_Article = '$id_Article'";
+        mysqli_query($conn, $sql);
+           
+    }
+         if(isset($_POST['return'])){
+          $id_Article = $_POST['return'];
+          $sql = "DELETE FROM Archive WHERE Id_Article = '$id_Article'";
+          mysqli_query($conn, $sql);
+        }
+
+        afficheArtcile($conn);
+        ?>
+
+</div>
+</div>
+
+
+
 
 
     </main>
   </div>
-<?php
-  if(isset($_POST['Edit'])){
-          $id_user = $_POST['Edit'];
-          $sql = "SELECT * FROM User where Id_User = '$id_user'";
-          $result = mysqli_query($conn,$sql);
-          $row = mysqli_fetch_row($result);
-          $sqll = "SELECT Role_User from User where Role_User NOT LIKE '$row[4]'";
-          $resultt = mysqli_query($conn,$sqll);
-          $otherrole = mysqli_fetch_row($resultt);
-          
-        echo '
-        <div id="ArticleModel" class="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50 ">
-        <form id="ArticleForm" class="bg-white rounded-lg w-full max-w-[60rem] sm:max-w-3/4 md:max-w-2/3 p-4 sm:p-6 shadow-lg overflow-y-auto" method="POST" action="AdminUser.php"  >
-        <div class="flex justify-between items-center mb-4 sm:mb-6">
-        <h2 class="text-xl sm:text-2xl font-semibold text-gray-800">Nouvelle Article</h2>
-        <button  id="closearticlemodel" class="text-gray-500 hover:text-gray-700 focus:outline-none">
+
+
+
+
+  <div id="ArticleModel" class="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50 hidden">
+  <form id="ArticleForm" class="bg-white rounded-lg w-full max-w-[60rem] sm:max-w-3/4 md:max-w-2/3 p-4 sm:p-6 shadow-lg overflow-y-auto" method='POST' action='#' enctype="multipart/form-data" >
+    <div class="flex justify-between items-center mb-4 sm:mb-6">
+      <h2 class="text-xl sm:text-2xl font-semibold text-gray-800">Nouvelle Article</h2>
+      <button  id="closearticlemodel" class="text-gray-500 hover:text-gray-700 focus:outline-none">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
         </svg>
-        </button>
-        </div>
+      </button>
+    </div>
 
-        <div class="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-6">
-        <div class="w-full max-w-[50rem] sm:w-2/3 space-y-4">
+    <div class="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-6">
+      <!-- Left Side Inputs -->
+      <div class="w-full max-w-[50rem] sm:w-2/3 space-y-4">
+      <div class="flex flex-col">
+          <label for="Titre_Article" class="font-medium text-gray-600 text-sm sm:text-base">Title</label>
+          <input type="text" id="Titre_Article" name="Titre_Article" class="mt-2 p-2 sm:p-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Ex: First Principle of Machine Learning">
+        </div>
         <div class="flex flex-col">
-        <label for="Nmae_User" class="font-medium text-gray-600 text-sm sm:text-base">User Name</label>
-        <input type="text" id="Nmae_User" value="'.$row[1].'" name="Nmae_User" class="mt-2 p-2 sm:p-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Ex: First Principle of Machine Learning" readonly>
+          <label for="Contenu_Article" class="font-medium text-gray-600 text-sm sm:text-base">Contenu</label>
+          <textarea name="Contenu_Article" class="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" rows="4" placeholder="Machine learning is..."></textarea>
+          </div>
+ 
+      </div>
+
+      <!-- Right Side Inputs -->
+      <div class="w-full sm:w-1/3 bg-gray-100 rounded-lg p-4 space-y-4">
+        <div class="flex flex-col">
+          <label for="imagearticle" class="font-medium text-gray-600 text-sm sm:text-base">Image d'Article</label>
+          <input type="file" id="imagearticle" name="imagearticle" class="mt-2 p-2 sm:p-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
         </div>
         <div class="flex flex-col">
-        <label for="email_User" class="font-medium text-gray-600 text-sm sm:text-base">Email</label>
-        <input type="text" id="email_User" value="'.$row[3].'" name="email_User" class="mt-2 p-2 sm:p-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Ex: First Principle of Machine Learning" readonly>
+          <label for="status" class="font-medium text-gray-600 text-sm sm:text-base">Tage</label>
+          <select id="status" name="status" class="mt-2 p-2 sm:p-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
+            <option value="En Parc">tage1</option>
+            <option value="Sous Location">tage2</option>
+          </select>
         </div>
+     
+      </div>
+    </div>
 
-        </div>
-
-        <div class="w-full sm:w-1/3 bg-gray-100 rounded-lg p-4 space-y-4">
-        <div class="flex flex-col">
-        <label for="role" class="font-medium text-gray-600 text-sm sm:text-base">Role</label>
-        <select id="role" name="role" class="mt-2 p-2 sm:p-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
-        <option value="'.$row[4].'">'.$row[4].'</option>
-        <option value="'.$otherrole[0].'">'.$otherrole[0].'</option>
-        </select>
-        </div>
-
-        </div>
-        </div>
-
-        <div class="mt-6 sm:mt-8 flex justify-between space-y-4 sm:space-y-0 sm:flex-row">
-        <button id="closeModalBtn" class="bg-red-500 text-white py-2 sm:py-3 px-6 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500">Annuler</button>
-        <input type="submit" value="Valider" name="validateForm" id="validateForm" class="bg-green-600 text-white py-2 sm:py-3 px-6 rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500">
-        </div>
-        </form>
-        </div>
-          ';
-          
-        }
-
-        if(isset($_POST['validateForm'])){
-          $email_User = $_POST['email_User'];
-          $role = $_POST['role'];
-          $sql = "UPDATE User SET Role_User='$role' where Email_User = '$email_User'";
-          mysqli_query($conn,$sql);
-          echo "<script>window.location.href = window.location.href;</script>";
-
-        }
-
-?>
-
-
-
+    <div class="mt-6 sm:mt-8 flex justify-between space-y-4 sm:space-y-0 sm:flex-row">
+      <button id="closeModalBtn" class="bg-red-500 text-white py-2 sm:py-3 px-6 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500">Annuler</button>
+      <input type="submit" value="Valider" name="validateForm" id="validateForm" class="bg-green-600 text-white py-2 sm:py-3 px-6 rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500">
+    </div>
+  </form>
+</div>
 </body>
 <script src="../../public/assets/js/pages.js"></script>
 
